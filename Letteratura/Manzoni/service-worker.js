71f252dba182e0323167ce935f63433dffa54cbd
gb69prof <gb69prof@users.noreply.github.com>
2026-08-29 09:44:11 +0200

    Mantiene raggiungibili i comandi del taccuino

--- Letteratura/Manzoni/service-worker.js
+++ Letteratura/Manzoni/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "manzoni-pwa-";
-const CACHE_NAME = `${CACHE_PREFIX}v9`;
+const CACHE_NAME = `${CACHE_PREFIX}v10`;
 
 const LOCAL_ASSETS = [
   "./",
@@ -8,8 +8,8 @@ const LOCAL_ASSETS = [
   "./video.html",
   "./offline.html",
   "./manifest.json",
-  "./assets/css/style.css?v=9",
-  "./assets/js/app.js?v=9",
+  "./assets/css/style.css?v=10",
+  "./assets/js/app.js?v=10",
   "./assets/immagini/index.png",
   "./assets/immagini/manzoni-ritratto.png",
   "./assets/immagini/icon-192.png",
