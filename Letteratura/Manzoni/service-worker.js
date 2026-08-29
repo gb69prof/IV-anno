a68fb8bf31b56cdc2dd5ce46c7dab48c61b2011c
gb69prof <gb69prof@users.noreply.github.com>
2026-08-29 09:28:40 +0200

    Versiona gli asset finali di Manzoni

--- Letteratura/Manzoni/service-worker.js
+++ Letteratura/Manzoni/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "manzoni-pwa-";
-const CACHE_NAME = `${CACHE_PREFIX}v5`;
+const CACHE_NAME = `${CACHE_PREFIX}v6`;
 
 const LOCAL_ASSETS = [
   "./",
@@ -8,8 +8,8 @@ const LOCAL_ASSETS = [
   "./video.html",
   "./offline.html",
   "./manifest.json",
-  "./assets/css/style.css?v=5",
-  "./assets/js/app.js?v=5",
+  "./assets/css/style.css?v=6",
+  "./assets/js/app.js?v=6",
   "./assets/immagini/index.png",
   "./assets/immagini/manzoni-ritratto.png",
   "./assets/immagini/icon-192.png",
