d78787ca7a8489f59fd7122edaea56631e1262b6
gb69prof <gb69prof@users.noreply.github.com>
2026-08-29 07:37:13 +0200

    Aggiorna cache PWA Foscolo alla versione 13

--- Letteratura/Foscolo/service-worker.js
+++ Letteratura/Foscolo/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "foscolo-pwa-";
-const CACHE_NAME = `${CACHE_PREFIX}v12`;
+const CACHE_NAME = `${CACHE_PREFIX}v13`;
 
 const LOCAL_ASSETS = [
   "./",
@@ -7,9 +7,9 @@ const LOCAL_ASSETS = [
   "./mappe.html",
   "./video.html",
   "./manifest.json",
-  "./assets/css/style.css?v=12",
-  "./assets/js/app.js?v=12",
-  "./assets/js/study-workspace.js?v=12",
+  "./assets/css/style.css?v=13",
+  "./assets/js/app.js?v=13",
+  "./assets/js/study-workspace.js?v=13",
   "./assets/immagini/Foscolo-foto.PNG",
   "./assets/immagini/index.png",
   "./assets/mappe/Alla-sera.PNG",
